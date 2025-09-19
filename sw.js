bb77505b9b0debfb84374837493bf5e7a945c10c
GurikaK77 <223002836+GurikaK77@users.noreply.github.com>
2025-09-19 21:14:00 +0400

    poxui

--- sw.js
+++ sw.js
@@ -1,14 +1,9 @@
 const CACHE_NAME = 'my-website-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/styles.css',
-  '/script.js',
-  '/image.png'
-  // დაამატეთ ყველა ფაილი, რომლის დამახსოვრებაც გსურთ
+  '/index.html'
 ];
 
-// Service Worker-ის ინსტალაციისას
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
@@ -19,17 +14,13 @@ self.addEventListener('install', event => {
   );
 });
 
-// ქსელის მოთხოვნების დასაჭერად
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // თუ მოთხოვნა ქეშშია, აბრუნებს ქეშირებულ ვერსიას
         if (response) {
           return response;
         }
-
-        // თუ არა, აგზავნის მოთხოვნას ქსელში
         return fetch(event.request);
       })
   );
