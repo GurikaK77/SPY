3f548dfe40a1d88530df1a40eb1edd381d1dbb85
GurikaK77 <223002836+GurikaK77@users.noreply.github.com>
2025-09-23 18:21:38 +0400

    pwa-fix

--- sw.js
+++ sw.js
@@ -1,62 +1,32 @@
-const CACHE_NAME = 'spy-game-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://fonts.googleapis.com/css?family=Cinzel:400,700|Raleway:300,400,600&display=swap',
-  'https://www.soundjay.com/buttons/sounds/beep-07.mp3'
-];
-
-// --- Install Event: ქეშირება ფაილების ---
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache:', CACHE_NAME);
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// --- Activate Event: ძველი ქეშების წაშლა ---
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log('Deleting old cache:', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
 // --- Fetch Event: ქსელიდან თუ არა -> ქეშიდან ---
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
+        // თუ ქეშშია, დააბრუნე ქეშიდან
         if (response) {
           return response;
         }
 
+        // თუ ქეშში არ არის, სცადე ქსელიდან ჩამოტვირთვა
         return fetch(event.request)
           .then(fetchResponse => {
+            // შეამოწმეთ, არის თუ არა პასუხი ვალიდური
             if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
               return fetchResponse;
             }
 
+            // ვალიდური პასუხი ქეშში შეინახე
             const responseClone = fetchResponse.clone();
             caches.open(CACHE_NAME).then(cache => {
               cache.put(event.request, responseClone);
             });
+
             return fetchResponse;
           })
           .catch(() => {
+            // თუ ქსელიდან ჩამოტვირთვა ვერ მოხერხდა (მაგალითად, ოფლაინ ხართ),
+            // დააბრუნე ქეშიდან index.html
             return caches.match('/index.html');
           });
       })
