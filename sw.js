edbe0692fc6636e67657613085bdab48a1f42c99
GurikaK77 <223002836+GurikaK77@users.noreply.github.com>
2025-09-27 22:31:05 +0400

    tylapi

--- sw.js
+++ sw.js
@@ -1,17 +1,67 @@
+// *** 1. ქეშის ვერსია და ფაილების სია ***
+// !!! გაზარდეთ ეს ნომერი ყოველ ჯერზე, როცა კოდს შეცვლით (მაგ., v5 -> v6) !!!
+const CACHE_NAME = 'spy-cache-v5'; 
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js',
+  // თუ იყენებთ, დაამატეთ ხმის ფაილის ლინკიც
+  'https://www.soundjay.com/buttons/sounds/beep-07.mp3' 
+];
+
+// --- Install Event: ქეშის შექმნა და ფაილების შენახვა ---
+self.addEventListener('install', event => {
+  self.skipWaiting(); 
+  
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache); 
+      })
+      .catch(err => {
+          console.error('Failed to cache resources:', err);
+      })
+  );
+});
+
+// --- Activate Event: ძველი ქეშების გასუფთავება (უზრუნველყოფს განახლებას!) ---
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          // თუ ქეშის სახელი არ არის თეთრ სიაში (ანუ ძველი ვერსიაა), წაშალე
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim(); 
+});
+
+
 // --- Fetch Event: ქსელიდან თუ არა -> ქეშიდან ---
 self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // თუ ქეშშია, დააბრუნე ქეშიდან
+        // 1. თუ ქეშშია, დააბრუნე ქეშიდან
         if (response) {
           return response;
         }
 
-        // თუ ქეშში არ არის, სცადე ქსელიდან ჩამოტვირთვა
+        // 2. თუ ქეშში არ არის, სცადე ქსელიდან ჩამოტვირთვა
         return fetch(event.request)
           .then(fetchResponse => {
-            // შეამოწმეთ, არის თუ არა პასუხი ვალიდური
             if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== 'basic') {
               return fetchResponse;
             }
@@ -25,7 +75,7 @@ self.addEventListener('fetch', event => {
             return fetchResponse;
           })
           .catch(() => {
-            // თუ ქსელიდან ჩამოტვირთვა ვერ მოხერხდა (მაგალითად, ოფლაინ ხართ),
+            // 3. თუ ქსელიდან ჩამოტვირთვა ვერ მოხერხდა (ოფლაინ ხართ),
             // დააბრუნე ქეშიდან index.html
             return caches.match('/index.html');
           });
